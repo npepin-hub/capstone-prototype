5d0228e84d201d05b59ebc83abc076f53625a8dc
Nadege PEPIN <nadege.rique@gmail.com>
2020-04-05 23:57:49 -0400

    Functionnal multi thread script for data import

--- src/extraction.py
+++ src/extraction.py
@@ -1,23 +1,78 @@
 import pandas as pd
 import requests
+import PIL
 from PIL import Image
+import time
 import logging
 import io
 import os
 from pathlib import Path
 import h5py
 import numpy as np
+import concurrent.futures
+from threading import BoundedSemaphore
 import storage
 
-def request_data_and_store(dataframe, size, set_name, batch_size = 5):
-    """ Given a panda dataframe containing a list of urls and their corresponding caption, call 
-        the urls and stores each thumbnailed-padded-to-size Image/Caption into a single hdf5 file.
+
+def get_image(index, url, size, set_name):
+
+    logger = logging.getLogger()
+    logger.info(set_name+"-- Fetching URL#"+str(index))
+
+    # Gets URLs
+    try:
+        r = requests.get(url)
+
+        logger.info(set_name+"-- URL#"+str(index)+" Http code: "+str(r.status_code))
+        if (r.status_code == 200):
+            logger.info(url)
+            img = Image.open(io.BytesIO(r.content))
+            img.thumbnail(size, Image.ANTIALIAS)
+
+            padded_image = Image.new("RGB", size)
+            padded_image.paste(img, (int((size[0] - img.size[0])/2), int((size[1] - img.size[1])/2)))
+
+            #plt.imshow(np.asarray(padded_image))
+            #plt.show()
+
+            return int(r.status_code), padded_image
+        else:
+            return int(r.status_code), None
+
+    except (requests.exceptions.ConnectionError, requests.exceptions.InvalidURL, requests.exceptions.SSLError, requests.exceptions.ContentDecodingError) as e:
+        logger.info(set_name+"-- URL#"+str(index)+" Http error: "+str(e))
+
+    except PIL.UnidentifiedImageError as e:
+        logger.info(set_name+"-- URL#"+str(index)+" Http error: "+str(e))
+
+    except OSError as e:
+        logger.info(set_name+"-- URL#"+str(index)+" Http error: "+str(e))
+
+    return 500, None    
+
+
+
+def store_handler(set_name, index, caption, status_code, padded_image=None):
+    logger = logging.getLogger()
+    logger.info(set_name+"--Handler URL#"+str(index)+" Http code: "+str(status_code))
+
+    if (int(status_code) == 200):
+        storage.store_image(set_name, index, padded_image, caption)            
+    else:
+        storage.store_status(set_name, index, str(status_code)) 
+    return 
+
+
+
+def request_data_and_store(dataframe, size, set_name, start_index = 0):
+    """ Given a panda dataframe containing a list of urls and their corresponding caption, get 
+        the images and stores each thumbnailed-padded-to-size Image/Caption into a single hdf5 file.
         Parameters:
         ---------------
-        dataframe    the pandas dataframe raw dataset
+        dataframe    the pandas raw dataset
         size         the targeted size of the image in the form of a tuple (height, width)
-        batch_size   future batch size...
-        set_name     validation or training 
+        set_name     validation or training
+        start_index  the index at which the insertion will start
 
         Returns:     
         ----------
@@ -25,30 +80,26 @@ def request_data_and_store(dataframe, size, set_name, batch_size = 5):
     """
     logger = logging.getLogger()
 
-    # TODO - work with batches - For now, we will work on the batch_size-th first data of the validation dataset
-    for index, row in dataframe.iterrows():
-        if not(storage.exist(set_name ,index)):
+    queue = BoundedSemaphore(100)
+    
+    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:    
+        for index, row in dataframe.iterrows():
+            if (index % 1000) == 0:
+                logger.info("--Processing " + str(index))
 
-            # Gets URLs
-            r = requests.get(row.url)
-            logger.info(set_name+"-- URL#"+str(index)+" Http code: "+str(r.status_code))
-            if (r.status_code == 200):
-                logger.debug(row.url)
-                img = Image.open(io.BytesIO(r.content))
-                img.thumbnail(size, Image.ANTIALIAS)
+            if index > (start_index - 500) and not(storage.exist(set_name ,index)):
 
-                padded_image = Image.new("RGB", size)
-                padded_image.paste(img, (int((size[0] - img.size[0])/2), int((size[1] - img.size[1])/2)))
-                print(row.caption)
-                #plt.imshow(np.asarray(padded_image))
-                #plt.show()
-                # Stores the image into a hdf5 file
+                response_handler =  \
+                    lambda future, index=index, caption=row.caption: store_handler(set_name, index, caption, future.result()[0], future.result()[1])
 
-                storage.store_image(set_name, index, padded_image, row.caption)
+                release_handler = lambda future: queue.release()
                 
-            else:
-                storage.store_status(set_name, index, str(r.status_code))
+                logger.info("-- Before ACQUIRE -- " + str(index))
+                queue.acquire()
                 
-            dataframe.at[index, "statuscode"] = int(r.status_code)
+                logger.info("-- Before SUBMIT -- " + str(index))
+                future_image = executor.submit(get_image, index, row.url, size, set_name)
+                future_image.add_done_callback(response_handler)
+                future_image.add_done_callback(release_handler)
 
     return
--- src/storage.py
+++ src/storage.py
@@ -6,91 +6,168 @@ import os
 from pathlib import Path
 import h5py
 import numpy as np
+import logging
+from filelock import FileLock
 
-def exist(set_name ,index):
-    file = h5py.File(Path('../data/img/'+set_name+'.h5') , "a")
-    if (str(index) in file):
-        return True
+def get_path(set_name ,index):    
+    sub_set = int(int(index) / 100000)
+    file_path = Path('../data/img/'+set_name+'_'+str(sub_set)+'.h5')
+    lock_path = Path('../data/img/'+set_name+'_'+str(sub_set)+'.h5.lock')
+    
+    return file_path, lock_path
+
+
+def exist(set_name ,index):        
+    logger = logging.getLogger()
+
+    file_path, lock_path = get_path(set_name ,index)
+    # Locks the file since it will be accessed from multiple threads
+    lock = FileLock(lock_path)
+    lock.acquire()  
+    file = None
+    try:      
+        file = h5py.File(file_path , "r")
+        if (str(index) in file):
+            return True
+
+    except OSError as e:
+        logger.info(set_name+"-- URL#"+str(index)+" Error: "+str(e))
+    finally:
+        if file != None:
+            file.close()
+        lock.release()
 
     return False
 
 def store_status(set_name, index, status_code):
-    # Create/retrieve a new HDF5 file
-    file = h5py.File(Path('../data/img/'+set_name+'.h5') , "a")
+    logger = logging.getLogger()
+    logger.info(set_name+"-- URL#"+str(index)+" Storing status--- "+str(status_code))
     
-    # Remove image_id from file if already exists
-    if (str(index) in file):
-        del file[str(index)]
+    file_path, lock_path = get_path(set_name ,index)
+    # Locks the file since it will be accessed from multiple threads
 
-    # Create a group/dataset in the file for a given image/caption
-    group = file.create_group(str(index))
-    meta_set = group.create_dataset(
-        "status", data=status_code
-    )
-    
-    file.close()    
+    lock = FileLock(lock_path)
+    lock.acquire()  
+    file = None
+    try:                
+        # Create/retrieve a new HDF5 file
+        file = h5py.File(file_path , "a") 
+        
+        # Remove image_id from file if already exists
+        if (str(index) in file):
+            del file[str(index)]
 
-    return
+        # Create a group/dataset in the file for a given image/caption
+        group = file.create_group(str(index))
+        meta_set = group.create_dataset(
+            "status", data=status_code
+        )
+    except (OSError, Exception) as e:
+        logger.info(set_name+"-- URL#"+str(index)+" Error: "+str(e))
+    finally:
+        if file != None:
+            file.close()
+        lock.release()
 
 
 def store_image(set_name, index, image, caption):
-    """ Stores a single image and its caption.
+    """ Stores a single image/caption under the index in a given set_name storage
         Parameters:
         ---------------
-        image       image to be stored
-        index       integer unique ID for image
+        set_name    validation or training
+        index       integer unique ID for image 
+        image       image to be stored   
         caption     image caption
-        set_name    validation or training 
+        
     """
-    # Create/retrieve a new HDF5 file
-    file = h5py.File(Path('../data/img/'+set_name+'.h5') , "a")
-    
-    # Remove index from file if already exists
-    if (str(index) in file):
-        del file[str(index)]
+    logger = logging.getLogger()
+    logger.info(set_name+"-- URL#"+str(index)+" Storing image--- ")
+    file_path, lock_path = get_path(set_name ,index)
+    # Locks the file since it will be accessed from multiple threads
 
-    # Create a group/dataset in the file for a given image/caption
-    group = file.create_group(str(index))
-    group.create_dataset(
-        "image", np.shape(image), h5py.h5t.STD_U8BE, data=np.asarray(image)
-    )
-    group.create_dataset(
-        "caption", data=caption
-    )
-    group.create_dataset(
-        "status", data="200"
-    )
-    
-    file.close()    
+    lock = FileLock(lock_path)
+    lock.acquire()  
+    file = None    
+    try:        
+        # Create/retrieve a new HDF5 file
+        file = h5py.File(file_path , "a")
+               
+        # Remove index from file if already exists
+        if (str(index) in file):
+            del file[str(index)]
+
+        # Create a group/dataset in the file for a given image/caption
+        group = file.create_group(str(index))
+        group.create_dataset(
+            "image", np.shape(image), h5py.h5t.STD_U8BE, data=np.asarray(image)
+        )
+        group.create_dataset(
+            "caption", data=caption
+        )
+        group.create_dataset(
+            "status", data="200"
+        )
+    except (OSError, Exception) as e:
+        logger.info(set_name+"-- URL#"+str(index)+" Error: "+str(e))
+    finally:
+        if file != None:
+            file.close()
+        lock.release()
+
 
 
 def read_image(set_name, index):
-    """ Reads a single Image/Caption out of a storage
+    """ Reads a single Image/Caption given its index out of a set_name storage
         Parameters:
         ---------------
-        image_id    integer unique ID for image
         set_name    validation or training 
+        index       integer unique ID for image
 
         Returns:
         ----------
+        status      http status code
         image       image array stored
-        label       associated meta data, int label
+        caption     associated caption
     """
     # Open the HDF5 file
-    file = h5py.File(Path('../data/img/'+set_name+'.h5'), "r+")
+    file_path, lock_path = get_path(set_name ,index)
+    file = h5py.File(file_path, "r")
 
     group = file[str(index)]
 
-
     status = np.array(group["status"]).astype("str")
-    print(type(status))
     if (int(status) == 200):
         image = np.array(group["image"]).astype("uint8")
         caption = np.array(group["caption"]).astype("str")
         return status, image, caption
     
     return status, None, None
- 
 
  
+def get_statuses(set_name):
+    
+    logger = logging.getLogger()
+    file_path, lock_path = get_path(set_name ,index)
+    file = h5py.File(file_path, "r")
+    df = pd.DataFrame(columns = ['statuscode'])
+    df.astype({'statuscode': 'int32'})
+
+    for group in file:
+        if (int(group) % 1000) == 0:
+            logger.info("--GROUP " + str(group))
+        df.at[group, "statuscode"] = int(np.array(file[group]["status"]).astype("str"))
+    
+    return df
+
+
+def get_last(set_name):
+    
+    logger = logging.getLogger()
+    
+    file_path, lock_path = get_path(set_name ,index)
+    file = h5py.File(file_path, "a")
+    keys_list = list(file.keys())
+    logger.info("--KeysList size-- " + str(len(keys_list)))
+
+    return len(keys_list)
 
